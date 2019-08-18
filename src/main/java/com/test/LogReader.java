4566a08a574ae7d4e3f3adfcd3083fc110bdcf36
Damian Wojakowski <wojakowski.damian@gmail.com>
2019-08-18 19:16:31 +0200

    file reader

--- src/main/java/com/test/LogReader.java
+++ src/main/java/com/test/LogReader.java
@@ -1,4 +1,44 @@
 package com.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LogReader {
+    private FileInputStream inputStream;
+    private Scanner scanner;
+    private static final String charset = "UTF-8";
+
+    public void loadFile(String pathToTestLogs) throws IOException {
+        try {
+            inputStream = new FileInputStream(pathToTestLogs);
+            scanner = new Scanner(inputStream, charset);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+
+    public boolean hasNextLine() {
+        return scanner.hasNextLine();
+    }
+
+    public String getNextLine() {
+        return scanner.nextLine();
+    }
+
+    public void closeStream() throws IOException {
+        if (inputStream != null) {
+            inputStream.close();
+        }
+        if (scanner != null) {
+            scanner.close();
+        }
+    }
 }
--- src/test/java/com/test/LogReaderTest.java
+++ src/test/java/com/test/LogReaderTest.java
@@ -2,11 +2,16 @@ package com.test;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class LogReaderTest {
-    LogReader logReader;
+    private LogReader logReader;
+    private static final String testLogsFilename = "logs";
 
     @Before
     public void setUp() throws Exception {
@@ -18,5 +23,28 @@ public class LogReaderTest {
         logReader = null;
     }
 
+    @Test
+    public void GivenFilePath_loadLogsFileAndReturnsWhetherCanRead() throws IOException {
+        logReader.loadFile(getPathToTestLogs());
 
+        assertTrue(logReader.hasNextLine());
+
+        logReader.closeStream();
+    }
+
+    @Test
+    public void GivenFilePath_loadReadsFirstLine() throws IOException {
+        logReader.loadFile(getPathToTestLogs());
+
+        logReader.hasNextLine();
+        assertNotNull(logReader.getNextLine());
+
+        logReader.closeStream();
+    }
+
+    private String getPathToTestLogs() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(testLogsFilename).getFile());
+        return file.getAbsolutePath();
+    }
 }
